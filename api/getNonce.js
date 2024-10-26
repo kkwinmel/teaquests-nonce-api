ad831fc29a0994f642c4dcdfeb8e5b2be12d66ae
kkwinmel <128441485+kkwinmel@users.noreply.github.com>
2024-10-26 15:57:38 +1100

    Update getNonce.js

--- api/getNonce.js
+++ api/getNonce.js
@@ -1,21 +1,21 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://teaquests.com/wp-json/wc/store/v1/cart?consumer_key=ck_faf6e983fe22bdbf14cef66483891058dcaf17c0&consumer_secret=cs_f41ecc320c623f095ef2da62e2af83f353359136', {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include'  // 加入呢行
-    });
+    const response = await fetch(
+      `https://teaquests.com/wp-json/wc/store/v1/cart?consumer_key=${process.env.CONSUMER_KEY}&consumer_secret=${process.env.CONSUMER_SECRET}`, 
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+      }
+    );
     
-    // 儲存返 cookie
     const cookies = response.headers.get('set-cookie');
-    
     const headers = Object.fromEntries(response.headers);
     const nonce = headers['nonce'];
     
-    // 將 cookie 同 nonce 一齊返回
     res.status(200).json({ 
       nonce,
       cookies 
